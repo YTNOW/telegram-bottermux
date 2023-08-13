c495f2d533c259759e9eb44d49c65a5d33b92e5d
ALAS <139692597+YTNOW@users.noreply.github.com>
2023-08-13 21:57:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('6158932010:AAHmV0jWVztbGM67oz-MSy9aU6rY37Nen88')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
